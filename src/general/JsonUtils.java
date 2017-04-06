db3e9f27525b18a860551014a0978dde185e220d
EgorDm <egordmitriev2@gmail.com>
2017-04-06 23:55:44 +0200

    Utils gewijzigd nu is tijd ook in millisec

---
+++ src/general/JsonUtils.java
@@ -0,0 +1,22 @@
+package general;
+
+import javax.json.Json;
+
+/**
+ * Created by EgorDm on 06-Apr-2017.
+ */
+public class JsonUtils {
+
+    private static final String successMessage = Json.createObjectBuilder().add("success", true).build().toString();
+
+    public static String getErrorMessage(String message) {
+        return Json.createObjectBuilder()
+                .add("success", false)
+                .add("message", message)
+                .build().toString();
+    }
+
+    public static String getSuccessMessage() {
+        return successMessage;
+    }
+}
